308a5547ec901ad4b29d1e5c7ab4e850855fb001
Parama Danoesubroto <parama@answersengine.com>
2018-10-07 03:02:37 -0400

    change seeder to point to the 1 million record of csv file

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,12 +1,12 @@
-CSV.foreach("./seeder/majestic_million_small.csv",:headers => true) do |row|
-    pages << {
-      url: "https://#{row['Domain']}",
-      page_type: 'home',
-    }
-  
-    # Save pages to the job partially to not load everything into memory
-    max_records = 100
-    save_pages(pages) if $. % max_records == 0
-  end
+CSV.foreach("./seeder/majestic_million.csv",:headers => true) do |row|
+  pages << {
+    url: "https://#{row['Domain']}",
+    page_type: 'home',
+  }
+
+  # Save pages to the job partially to not load everything into memory
+  max_records = 100
+  save_pages(pages) if $. % max_records == 0
+end
   
   
